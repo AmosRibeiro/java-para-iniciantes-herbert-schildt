616954967d98fb9f4751bf193b14ea694ca9fcad
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-26 17:13:10 -0300

    Inclusão de três codigos Iniciantes de Java.

---
+++ Example.java
@@ -0,0 +1,12 @@
+/*
+	Este é um programa Java simples.
+	
+	Chame este arquivo de Example.java.
+*/
+
+public class Example{	
+
+	public static void main (String args[]){
+		System.out.println("Java Drives the Web.");
+	}
+}
