bae4598e915ea35fee1d333f9da6871bd1f8256c
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-29 16:38:25 -0300

    Programa que converte a unidade de medida galões na de litros.

---
+++ GalParaLit.java
@@ -0,0 +1,17 @@
+/* 
+    Tente isto 1-1
+
+    Este programa converte galões em litros.
+
+*/
+public class GalParaLit {
+    public static void main (String args[]){
+        double galoes; //número de galões
+        double litros; //conversão para litros
+
+        galoes = 10; 
+        litros = galoes * 3.7854;
+
+        System.out.println(galoes + " galões é igual a " + litros + " litros.");
+    }
+}
