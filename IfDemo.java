ee2f07fa919df8988262c02f5a72ca0d2be34ac3
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-29 16:58:09 -0300

    demonstração do uso de IF

---
+++ IfDemo.java
@@ -0,0 +1,29 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 2;
+        b = 3;
+
+        if(a < b) System.out.println("A é menor que B."); 
+        
+
+
+        if(a == b) System.out.println("Você não verá isso."); //esta instrução não será exibida
+
+        System.out.println();
+
+        c = a - b; // c contém -1
+
+        System.out.println("C contém o valor de -1.");
+        if(c >= 0) System.out.println("C não é um valor negativo.");
+        if(c <0) System.out.println("C é um valor negativo.");
+
+        c = b - a; //c contém 1
+
+        System.out.println("C contém o valor de 1.");
+        if(c >= 0) System.out.println("C não é um valor negativo.");
+        if(c <0) System.out.println("C é um valor negativo.");
+    }
+    
+}
