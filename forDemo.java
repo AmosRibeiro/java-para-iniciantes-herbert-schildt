0035d1b64f9138366dd57f181cb7d3d641de272f
AmosRibeiro <amosribeiro2708@gmail.com>
2024-07-29 17:12:17 -0300

    uso do laço for em uma contagem

---
+++ forDemo.java
@@ -0,0 +1,11 @@
+public class forDemo {
+    public static void main(String[] args) {
+        int contar;
+
+        for(contar = 0; contar < 5; contar++)
+        System.out.println("Contagem atual: " + contar);
+
+        System.out.println("Fim.");
+    }
+    
+}
