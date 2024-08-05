abfc97b96a7261d3dfd0369df38864d5adb59215
AmosRibeiro <amosribeiro2708@gmail.com>
2024-08-05 15:36:12 -0300

    sequencia de escape em strings

---
+++ CAPITULO-2/StrDemo.java
@@ -0,0 +1,7 @@
+public class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("Primeira linha \nSegunda linha");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
