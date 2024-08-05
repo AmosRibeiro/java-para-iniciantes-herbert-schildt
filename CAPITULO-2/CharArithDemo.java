ef95af7adf0e05070c3a4dc4a70fac80d8f28543
AmosRibeiro <amosribeiro2708@gmail.com>
2024-08-05 14:34:16 -0300

    capitulo 2 demonstrações

---
+++ CAPITULO-2/CharArithDemo.java
@@ -0,0 +1,15 @@
+// Variáveis de caracteres podem ser tratadas como inteiros.
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("A variável ch contém: " + ch);
+
+        ch++; //incrementa ch
+        System.out.println("ch agora é: " + ch);
+
+        ch=90; //dá a ch o valor de z
+        System.out.println("ch é: " + ch);
+    }
+}
