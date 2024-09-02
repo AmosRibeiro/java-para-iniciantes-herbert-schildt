fc4fa02677c508938ffbfcc7e9fdb08958241c49
AmosRibeiro <amosribeiro2708@gmail.com>
2024-09-02 15:59:36 -0300

    melhorias nos programas com o uso de do-while

--- CAPITULO-3/Help.java
+++ CAPITULO-3/Help.java
@@ -4,33 +4,58 @@
 public class Help {
     public static void main(String[] args) 
         throws java.io.IOException{
-            char choice;
+            char choice, ignore;
 
+            do {
             System.out.println("Você precisa de ajuda com:");
             System.out.println("   1. if");
             System.out.println("   2. switch");
+            System.out.println("   3. for");
+            System.out.println("   4. while");
+            System.out.println("   5. do-while\n");
             System.out.println("Escolha um: ");
             choice = (char) System.in.read();
 
-            System.out.println("\n");
+            
+            do {
+                ignore = (char) System.in.read();
+               } while (ignore != '\n');
+            } while (choice < '1' | choice > '5');
 
             switch (choice) {
                 case '1':
-                    System.out.println("O if:\n");
+                    System.out.println("Estrutura básica da instrução if:\n");
                     System.out.println("if(condição) instrução;");
                     System.out.println("else intrução;");
                     break;
+        
                 case '2':
-                    System.out.println("O switch:\n");
+                    System.out.println("Estrutura básica da instrução switch:\n");
                     System.out.println("switch(expressão){");
                     System.out.println("  case constante:");
-                    System.out.println("    sequencia de instrução");
+                    System.out.println("    sequência de instrução");
                     System.out.println("     break;");
                     System.out.println(" // ...");
                     System.out.println("}");
                     break;
-                default:
-                   System.out.println("Opção selecionada inválida.");
+
+                case '3':
+                    System.out.println("Estrutura básica do laço for:\n");
+                    System.out.print("for(inicialização; condição; iteração);");
+                    System.out.println(" instrução;");
+                    break;
+
+                case '4':
+                    System.out.println("Estrutura básica do laço while:\n");
+                    System.out.println("while(condição) instrução;");
+                    break;
+
+                case '5':
+                    System.out.println("Estrutura básica do laço do-while:\n");
+                    System.out.println("do {");
+                    System.out.println(" instrução;");
+                    System.out.println("} while (condição);");
+                    break;
             }
         
     }
