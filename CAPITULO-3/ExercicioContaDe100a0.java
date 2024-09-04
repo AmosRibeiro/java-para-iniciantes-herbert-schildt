d7709e0e0b936c2b6d4d35c9d9b06f9f071def39
AmosRibeiro <amosribeiro2708@gmail.com>
2024-09-04 15:20:10 -0300

    mais exercicios do capitulo 3 resolvidos.

---
+++ CAPITULO-3/ExercicioContaDe100a0.java
@@ -0,0 +1,6 @@
+//Laço for que conta de 1000 até 0 em intervalos de -2.
+public class ExercicioContaDe100a0 {
+    public static void main(String[] args) {
+        for(int i = 1000; i >= 0 ; i = i - 2) System.out.println(i);
+    }
+}
