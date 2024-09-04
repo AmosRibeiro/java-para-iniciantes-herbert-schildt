dbd429ca28ae775de8efc2c8d26467d04c77bbf8
AmosRibeiro <amosribeiro2708@gmail.com>
2024-09-04 10:55:29 -0300

    uso de break e continue e rotulos

--- CAPITULO-3/Help.java
+++ CAPITULO-3/Help.java
@@ -1,26 +1,34 @@
 /*
- * Um sistema de ajuda simples
+ * Um sistema de ajuda simples versão 3
  */
 public class Help {
     public static void main(String[] args) 
         throws java.io.IOException{
             char choice, ignore;
 
-            do {
+            for(; ;){
+                do {
             System.out.println("Você precisa de ajuda com:");
             System.out.println("   1. if");
             System.out.println("   2. switch");
             System.out.println("   3. for");
             System.out.println("   4. while");
-            System.out.println("   5. do-while\n");
-            System.out.println("Escolha um: ");
+            System.out.println("   5. do-while");
+            System.out.println("   6. break");
+            System.out.println("   7. continue\n");
+            System.out.println("Escolha uma opção ou pressione q para sair: ");
+
             choice = (char) System.in.read();
 
             
             do {
                 ignore = (char) System.in.read();
                } while (ignore != '\n');
-            } while (choice < '1' | choice > '5');
+            } while (choice < '1' | choice > '7' & choice != 'q');
+
+            if(choice == 'q') break;
+
+            System.out.println("\n");
 
             switch (choice) {
                 case '1':
@@ -56,7 +64,18 @@ public class Help {
                     System.out.println(" instrução;");
                     System.out.println("} while (condição);");
                     break;
+
+                case '6':
+                    System.out.println("Estrutura básica do break:\n");
+                    System.out.println("break; ou break rótulo;");
+                    break;
+
+                case '7':
+                    System.out.println("Estrutura básica do continue:\n");
+                    System.out.println("continue; ou continue rótulo;");
+                    break;
             }
-        
+        System.out.println();
+        }
     }
 }
