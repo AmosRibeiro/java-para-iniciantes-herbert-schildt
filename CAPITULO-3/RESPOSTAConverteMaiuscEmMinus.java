d87a4535b2edd8b428bec3318e75f499a81dc7d7
AmosRibeiro <amosribeiro2708@gmail.com>
2024-09-04 15:39:53 -0300

    Resposta oficial do exercicio de converter maiusculas em minusculas

---
+++ CAPITULO-3/RESPOSTAConverteMaiuscEmMinus.java
@@ -0,0 +1,24 @@
+// a forma que respondi pode ser encontrada no arquivo ConverteMaiuscEmMinus.java, essa é a resposta dada pelo livro em que o código é mais simples.
+public class RESPOSTAConverteMaiuscEmMinus {
+    public static void main(String[] args) throws java.io.IOException{
+        char ch;
+        int changes = 0;
+    
+        System.out.println("Digite um ponto final para parar.");
+
+        do {
+            ch  = (char) System.in.read();
+            if(ch >= 'a' & ch <= 'z'){
+                ch -= 32;
+                changes++;
+                System.out.println(ch);
+            }
+            else if(ch >= 'A' & ch <= 'Z'){
+                ch += 32;
+                changes++;
+                System.out.println(ch);
+            }
+        } while (ch != '.');
+        System.out.println("Foram alteradas:" + changes);
+    }
+}
