79a662902dca57bbbb1f46c93c86c7528c517bb3
AmosRibeiro <amosribeiro2708@gmail.com>
2024-09-17 13:18:48 -0300

    mais usos de construtores

--- capitulo4/veiculo/Veiculo.java
+++ capitulo4/veiculo/Veiculo.java
@@ -5,10 +5,17 @@ public class Veiculo {
     int capacidadeCombustivel;
     int consumo;
 
+    //Construtor para veiculo.
+    Veiculo(int p, int f, int m){
+        passageiros = p;
+        capacidadeCombustivel = f;
+        consumo = m;
+    }
+    //Retorna autonomia do veiculo.
     int alcance(){
         return consumo * capacidadeCombustivel;
     }
-
+    //Calcula o combustivel necessaria para cobrir determinada distância.
     double combustivelNecessario(int kilometros){
         return (double) kilometros / consumo;
     }
--- capitulo4/veiculo/Veiculos.java
+++ capitulo4/veiculo/Veiculos.java
@@ -3,24 +3,18 @@ package capitulo4.veiculo;
 class Veiculos {
 
     public static void main(String[] args) {
-        Veiculo porche = new Veiculo();
-        Veiculo bmw = new Veiculo();
+        Veiculo porche = new Veiculo(7, 16, 21);
+        Veiculo bmw = new Veiculo(2, 14, 12);
 
         double litrosGasolina;
         int distancia = 200;
 
-
-        porche.passageiros = 2;
-        porche.capacidadeCombustivel = 10;
-        porche.consumo =8;
-
-        bmw.passageiros = 4;
-        bmw.capacidadeCombustivel = 18;
-        bmw.consumo = 10;
-
         litrosGasolina = porche.combustivelNecessario(distancia);
+
         System.out.println("A porche pode acomodar "+ porche.passageiros + " passageiros e tem um alcance de " + porche.alcance() + ". E para ir a uma distancia de " + distancia + " KM, precisa de " + litrosGasolina + " litros de gasolina.");
+        
         litrosGasolina = bmw.combustivelNecessario(distancia);
+        
         System.out.print("A bmw pode acomodar " + bmw.passageiros + " passageiros e tem um alcance de " + bmw.alcance() + ". E para ir a uma distancia de " + distancia + " KM, precisa de " + litrosGasolina + " litros de gasolina.");
        
     }
