0c6d0f6c20ea98fe33bff45197a56fd0806256f5
AmosRibeiro <amosribeiro2708@gmail.com>
2024-09-18 21:16:42 -0300

    resolucao exercicio cap4

---
+++ capitulo4/teste_capitulo_4/Exercicio4_1.java
@@ -0,0 +1,9 @@
+package capitulo4.teste_capitulo_4;
+
+public class Exercicio4_1 {
+    MyCounter count = new MyCounter(4, "Amos");
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ capitulo4/teste_capitulo_4/MyCounter.java
@@ -0,0 +1,12 @@
+package capitulo4.teste_capitulo_4;
+
+
+public class MyCounter {
+    int x = 1;
+    String name;
+
+    MyCounter(int x, String name){
+        this.x = x;
+        this.name = name;
+    }
+}
