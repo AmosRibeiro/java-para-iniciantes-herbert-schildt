4683aa1e82989743d675a7c7511ea037c87300e3
AmosRibeiro <amosribeiro2708@gmail.com>
2024-09-19 19:30:45 -0300

    uso de arrays e organizar por bolha

---
+++ capitulo5/Itens.java
@@ -0,0 +1,17 @@
+package capitulo5;
+
+public class Itens {
+    public static void main(String[] args) {
+        String itens[] = new String[10];
+
+        itens[0] = "Cajado";
+        itens[1] = "Adaga";
+        itens[2] = "Livro";
+        itens[3] = "Poção de HP";
+        itens[4] = "Poção de mana";
+        itens[5] = "Conjunto de roupa";
+
+        System.out.println("Você possui os seguintes itens: " + itens[0] + ", " + itens[1] + ", " + itens[2] + ", " + itens[3] + ", " + itens[4] + ", " + itens[5] + ".");
+
+    }
+}
