f64b6b04cdfb776b21fd56b0359fb63d25f72bc0
AmosRibeiro <amosribeiro2708@gmail.com>
2024-10-09 15:32:40 -0300

    Strings !!!

---
+++ capitulo5/SubStr.java
@@ -0,0 +1,13 @@
+package capitulo5;
+
+public class SubStr {
+    public static void main(String[] args) {
+        String orgstr = "Java makes the Web move.";
+
+        //constrói um substring
+        String substr = orgstr.substring(5, 18);
+
+        System.out.println("orgstr: " + orgstr);
+        System.out.println("substr: " + substr);
+    }
+}
