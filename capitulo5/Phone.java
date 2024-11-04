a086384e356bfacb54a7b4f8a0d4ead5ebde0632
Amós Ribeiro <amosribeiro2708@gmail.com>
2024-11-04 15:33:05 -0300

    uso de argumentos de linha de comando

---
+++ capitulo5/Phone.java
@@ -0,0 +1,26 @@
+package capitulo5;
+
+public class Phone {
+    public static void main(String[] args) {
+        String numbers[][] = {
+            {"Tom", "555-3322"},
+            {"Mary", "555-8976"},
+            {"Jon", "555-1037"},
+            {"Rachel", "555-1400"}
+        };
+        int i;
+
+        if(args.length != 1)
+            System.out.println("Coloque apenas um nome confome o exemplo: java Phone.java <nome>");
+        else {
+            for(i=0; i<numbers.length; i++){
+                if(numbers[i][0].equals(args[0])){
+                    System.out.println(numbers[i][0] + ": " + numbers[i][1]);
+                    break;
+                }
+            }
+            if (i == numbers.length) 
+                System.out.println("Nome não econtrado.");
+        } 
+    }
+}
