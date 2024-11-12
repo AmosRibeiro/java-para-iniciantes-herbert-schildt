801f0a5bc1b2823caa0808b477e4b5438b2e1784
Amós Ribeiro <amosribeiro2708@gmail.com>
2024-11-12 20:53:33 -0300

    modificadores de acesso public e private

---
+++ capitulo6/modificadoracess/MyClass.java
@@ -0,0 +1,20 @@
+package capitulo6.modificadoracess;
+
+// Acesso público versus privado
+public class MyClass {
+    private int alpha; // acesso privado
+    public int beta; // acesso público
+    int gamma; // acesso padrão
+
+    /*  Métodos para acessar alpha. Não há
+     * problema em um membro de uma classe acessar um membro
+     * privado da mesma classe.
+     */
+    void setAlpha(int a){
+        alpha = a;
+    }
+
+    int getAlpha() {
+        return alpha;
+    }
+}
