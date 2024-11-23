aacb430c8bc3ed44818a5c17545c96cada9f0d53
Amós Ribeiro <amosribeiro2708@gmail.com>
2024-11-23 15:31:44 -0300

    variaveis, metodos e blocos estáticos

---
+++ capitulo6/staticdemo/StaticDemo.java
@@ -0,0 +1,13 @@
+package capitulo6.staticdemo;
+
+public class StaticDemo {
+    int x; // Variável de instância comum.
+    static int y; // Variável estática.
+
+    int sum(){
+        return x + y;
+    }
+    static int sub(){ // Método estático, só pode acessar diretamente dados static.
+        return y - 1;
+    }
+}
