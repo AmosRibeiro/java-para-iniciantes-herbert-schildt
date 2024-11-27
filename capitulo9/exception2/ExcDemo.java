9f27998bc78776fc196627b3e3dcf70a176b527d
Amós Ribeiro <amosribeiro2708@gmail.com>
2024-11-27 16:37:30 -0300

    exceptions

---
+++ capitulo9/exception2/ExcDemo.java
@@ -0,0 +1,12 @@
+package capitulo9.exception2;
+
+public class ExcDemo {
+    public static void main(String[] args) {
+        try{
+            ExcTest.genException();
+        } catch(ArrayIndexOutOfBoundsException exc){
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement.");
+    }
+}
---
+++ capitulo9/exceptionexample/ExcDemo1.java
@@ -0,0 +1,17 @@
+package capitulo9.exceptionexample;
+
+public class ExcDemo1 {
+    public static void main(String[] args) {
+        int nums[] = new int [4];
+
+        try{
+
+            System.out.println("Before exception is generated.");
+
+            nums[7] = 10;
+        }catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement.");
+    }
+}
